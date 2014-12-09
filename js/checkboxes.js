e2b33ee7a830c096486bc04d8eb19b312f6089ed
drmyersii <drmyersii@users.noreply.github.com>
2014-12-09 04:13:35 -0500

    Checkbox support!
    Big update with none in between... Whoops. Checkbox support is now fully
    functional though! All sizes are in rem so things scale well!
    
    TODO: Add mixins for other browser prefixes
    
    Next up (after minor tweaks on this still): Radios!

---
+++ js/checkboxes.js
@@ -0,0 +1,46 @@
+;(function (win, doc, undefined)
+{ 'use strict';
+
+	var setEventListener = function (element, event, callback)
+	{
+		if (element.addEventListener)
+		{
+			element.addEventListener(event, callback, false);
+		}
+		else if (element.attachEvent)
+		{
+			element.attachEvent('on' + event, callback);
+		}
+	}
+
+	var makeMaterialCheckbox = function (normalCheckbox)
+	{
+		var materialCheckbox = doc.createElement('div');
+			materialCheckbox.setAttribute('class', 'checkbox checkbox-primary');
+
+		var ripple = doc.createElement('span');
+			ripple.setAttribute('class', 'ripple');
+
+		var check = doc.createElement('span');
+			check.setAttribute('class', 'check');
+
+		materialCheckbox.appendChild(normalCheckbox);
+		materialCheckbox.appendChild(ripple);
+		materialCheckbox.appendChild(check);
+
+		return materialCheckbox;
+	}
+
+	setEventListener(win, 'load', function ()
+	{
+		var checkboxes = doc.querySelectorAll('input[type="checkbox"]'), checkboxes_length = checkboxes.length;
+
+		for (var i = 0; i < checkboxes_length; ++i)
+		{
+			var normalCheckbox = checkboxes[i];
+			var materialCheckbox = makeMaterialCheckbox(normalCheckbox.cloneNode());
+
+			normalCheckbox.parentNode.replaceChild(materialCheckbox, normalCheckbox);
+		}
+	});
+})(window, document);
